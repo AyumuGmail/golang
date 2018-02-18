a26fd29455085aeda4a2125f6980094ec6720e18
kataoka.ayumu@gmail.com <kataoka.ayumu@gmail.com>
2018-02-18 14:35:30 +0900

    よくわからんが...

--- src/gopl.io/ch03/ex05/ex05.go
+++ src/gopl.io/ch03/ex05/ex05.go
@@ -43,7 +43,11 @@ func mainProc() {
 			z2 := complex(x+sampllingX, y)
 			z3 := complex(x, y+sampllingY)
 			z4 := complex(x+sampllingX, y+sampllingY)
-			img1.Set(px, py, mandelbrotSuperSamplling(z1, z2, z3, z4))
+			c1 := mandelbrot(z1)
+			c2 := mandelbrot(z2)
+			c3 := mandelbrot(z3)
+			c4 := mandelbrot(z4)
+			img1.Set(px, py, averrageColor(c1, c2, c3, c4))
 		}
 	}
 	out, _ := os.Create("img.png")
@@ -57,7 +61,7 @@ const (
 	RGBRed, RGBGreen, RGBBlue, RGBAlpha = 150, 15, 200, 0xff
 )
 
-func mandelbrot(z complex128) color.Color {
+func mandelbrot(z complex128) color.RGBA {
 	const iterations = 200
 	const contrast = 15
 
@@ -74,25 +78,14 @@ func mandelbrot(z complex128) color.Color {
 				RGBAlpha}
 		}
 	}
-	return color.Black
+	return color.RGBA{0, 0, 0, RGBAlpha} //color.Black
 }
 
-func mandelbrotSuperSamplling(z1 complex128, z2 complex128, z3 complex128, z4 complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
-
-	var v complex128
-	for n := uint8(0); n < iterations; n++ {
-		v = v*v + complex128((z1+z2+z3+z4)/4)
-		if cmplx.Abs(v) > 2 {
-			//return color.Gray{255 - contrast*n}
-			//フルカラーでも同じ色っていうのが正解?
-			//return color.RGBA{255 - contrast*n, 255 - contrast*n, 255 - contrast*n, 0xff}
-			return color.RGBA{RGBRed - contrast*n,
-				RGBGreen - contrast*n,
-				RGBBlue - contrast*n,
-				RGBAlpha}
-		}
+func averrageColor(color1 color.RGBA, color2 color.RGBA, color3 color.RGBA, color4 color.RGBA) color.Color {
+	return color.RGBA{
+		(color1.R + color2.R + color3.R + color4.R) / 4,
+		(color1.G + color2.G + color3.G + color4.G) / 4,
+		(color1.B + color2.B + color3.B + color4.B) / 4,
+		0xff,
 	}
-	return color.Black
 }
